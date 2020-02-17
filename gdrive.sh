05ae0755717ddb906d1b72c053a3981ce7c326c0
David Zavalla <davidzavalla@gmail.com>
2020-02-17 00:33:01 +0000

    Check if an argument is given

--- gdrive.sh
+++ gdrive.sh
@@ -1,3 +1,7 @@
-filename=$(wget -q "https://drive.google.com/file/d/$1/view" -O- | grep -oP "(?<=title': ').*?(?=',)" )
-
-wget --no-check-certificate -r "https://docs.google.com/uc?export=download&id=$1" -O "${filename}"
+if [ -z $1 ]
+then
+    echo "Usage: $0 [FILEID]. Example: $0 1QObMn1zwFHuHYnGP9rRXfIxIc57_o4LA"
+else
+    filename=$(wget -q "https://drive.google.com/file/d/$1/view" -O- | grep -oP "(?<=title': ').*?(?=',)" )
+    wget --no-check-certificate -r "https://docs.google.com/uc?export=download&id=$1" -O "${filename}"
+fi
